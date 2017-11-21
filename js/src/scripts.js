2f379f9cd05e53c775293c47da3d34cde7df1ef0
Fábio Z.C <fabiozc@gmail.com>
2017-11-21 19:12:11 +0100

    tilt effect for experiences

--- js/src/scripts.js
+++ js/src/scripts.js
@@ -1,4 +1,17 @@
+$(document).ready(function() {
+	introScrollEffect();
+	$('.js-exp-item').tilt({
+		scale: 1.05,
+		maxTilt: 8,
+		perspective: 1000,
+		tiltSpeed: 400
+	});
+});
 $(document).scroll(function() {
+	introScrollEffect();
+});
+
+function introScrollEffect(){
 	if(($(this).scrollTop() * 1.3) < $(window).height()){
 		$(".js-intro").css({
 			"opacity": 1 - (parseInt($(this).scrollTop())/(parseInt($(".js-intro").css("height"))/1.8))
@@ -8,4 +21,4 @@ $(document).scroll(function() {
 			"opacity": 0
 		});
 	}
-});
+}
