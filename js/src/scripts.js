ede16b457a57c930d9370986b3cd5946846dd522
Fábio Z.C <fabiozc@gmail.com>
2017-11-30 00:07:38 +0100

    experience modal is now opening flawlessly with overlay and also closing

--- js/src/scripts.js
+++ js/src/scripts.js
@@ -1,26 +1,3 @@
-$(document).ready(function() {
-	introScrollEffect();
-	$('.js-exp-item').tilt({
-		scale: 1.05,
-		maxTilt: 9,
-		perspective: 2000,
-		tiltSpeed: 400
-	});
-
-	$('.js-exp-item').on('click tap',$.proxy(expandExperience, self));
-	//$('.js-exp-item.is-open').on('click tap',closeExperience());
-
-
-	$(".js-scroll").click(function(e) {
-	    e.preventDefault();
-	    var aid = $(this).attr("href");
-	    $('html,body').animate({scrollTop: $(aid).offset().top - 330},'slow');
-	});
-});
-$(document).scroll(function() {
-	introScrollEffect();
-});
-
 function introScrollEffect(){
 	if(($(this).scrollTop() * 1.3) < $(window).height()){
 		$('.js-intro').css({
@@ -33,14 +10,19 @@ function introScrollEffect(){
 	}
 }
 
-$('.js-pagi').on('click', function (e) {
-	e.preventDefault();
-	if ($('.is-current').next().length == 0) {
-		$('.js-review-item').removeClass('is-current is-prev').first().addClass('is-current');
-		return;
-	}
-	$('.is-current').addClass("is-prev").removeClass('is-current').next().removeClass("is-prev").addClass('is-current');
-});
+function callOverlay(){
+	$('body').append("<div class='overlay js-overlay'><span class='modal-close js-modal-close' >&times;</span></div>").find('.js-overlay').animate({'opacity':'1'});
+}
+function destroyOverlay(){
+	var overlay = $(".overlay");
+	overlay.animate({
+		"opacity":"0"
+	}, function(){
+		overlay.remove();
+	});
+	$(".js-request").empty();
+	$(".js-exp-item").css("opacity","1");
+}
 
 function expandExperience(event){
 	var container = $('.js-request');
@@ -58,33 +40,68 @@ function expandExperience(event){
 	// 	return;
 	// }
 
+	callOverlay();
 	container.removeAttr('style').empty();
 
 	var getWitdh = $el.css('width');
+	var getTop = $el.offset().top - $(document).scrollTop();
 	var getLeft = $el.offset().left;
 
 	$el.clone().appendTo(container);
 	container.find('.exp-item').removeClass('.js-exp-item').removeAttr('style').addClass('is-open');
-	container.show().css({
-		'position' : 'absolute',
-		'top' : '-290px',
+	container.show().addClass("modal").css({
+		'position' : 'fixed',
+		'top' : getTop + 14,
 		'left' : getLeft,
 		'width': getWitdh
 	}).animate({
-		'bottom' : '10vh',
-		'top' : 'auto',
+		'top' : '10vh',
 		'left' : '10vw',
 		'width': '80vw',
-		'height': '80vh',
 		'position': 'fixed',
-		'transform': 'scale(1)'
 	},500);
 
-	$el.css({
-		'opacity': '0'
-	});
+	$el.css({'opacity': '0'});
 	container.find('.exp-item').children().animate({ 'opacity':'0'}, function(){ container.find('.exp-item').empty(); });
 }
 
-function closeExperience(){
-}
+//function closeExperience(){}
+
+$(document).ready(function() {
+	introScrollEffect();
+	$('.js-exp-item').tilt({
+		scale: 1.05,
+		maxTilt: 9,
+		perspective: 2000,
+		tiltSpeed: 400
+	});
+	$('.js-exp-item').on('click tap',$.proxy(expandExperience, self));
+	//$('.js-exp-item.is-open').on('click tap',closeExperience());
+
+
+	$(".js-scroll").on("click tap", function(e) {
+	    e.preventDefault();
+	    var aid = $(this).attr("href");
+	    $('html,body').animate({scrollTop: $(aid).offset().top - 330},'slow');
+	});
+});
+$('a[href="#"]').click( function(e) {
+	e.preventDefault();
+});
+$(document).on('click tap', '.js-modal-close, .js-overlay', function(){
+	destroyOverlay();
+});
+$(window).bind('keydown', function() {
+	if(event.keyCode === 27) {
+		destroyOverlay();
+	}
+});
+$(document).scroll(function() { introScrollEffect();});
+$('.js-pagi').on('click', function (e) {
+	e.preventDefault();
+	if ($('.is-current').next().length === 0) {
+		$('.js-review-item').removeClass('is-current is-prev').first().addClass('is-current');
+		return;
+	}
+	$('.is-current').addClass("is-prev").removeClass('is-current').next().removeClass("is-prev").addClass('is-current');
+});
