ad1009b0dc2afce9fbcbc3d9d3a403bef9fc794e
Fábio Z.C <fabiozc@gmail.com>
2018-01-07 03:41:51 +0100

    lazyload extras js and images

--- js/src/scripts.js
+++ js/src/scripts.js
@@ -1,3 +1,18 @@
+function require(script) {
+    $.ajax({
+        url: script,
+        dataType: "script",
+        async: false,           // <-- This is the key
+        success: function () {
+            // all good...
+        },
+        error: function () {
+            throw new Error("Could not load script " + script);
+        }
+    });
+}
+require("js/tilt.jquery-min.js");
+
 function introScrollEffect(){
 	var header = $('.js-intro');
 
